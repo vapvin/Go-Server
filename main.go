52370e02be9b9ecbf589a0c12bde1414bd07cbcf
vapvin <tpehdj12@daum.net>
2020-02-17 03:50:04 +0900

    Function on GO

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func plus(a, b int) int {
+	return a + b
+}
+
+func nameTat(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
 
 func main() {
 
 	// Constants
 	// const name string = "vins"
 	// Variables
-	var name1 string = "whos" // or
-	name2 := "vins"
-	fmt.Println(name1)
-	fmt.Println(name2)
+	// var name1 string = "whos" // or
+	// name2 := "vins"
+	lengs, upperName := nameTat("vins")
+	fmt.Println(plus(2, 2))
+	fmt.Println(lengs, upperName)
 }
