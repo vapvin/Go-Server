91a7f038d4831ba62fa0e1580841c18baa575aee
vapvin <tpehdj12@daum.net>
2020-03-10 18:37:59 +0900

    Get Text with Go

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
@@ -21,6 +29,22 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Request", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkRes(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find(".title>a").Text()
+		location := card.Find("sjcl").Text()
+		fmt.Println(id, title, location)
+	})
 }
 
 func getPages() int {
@@ -51,4 +75,8 @@ func checkRes(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Fail Status:", res.StatusCode)
 	}
+}
+
+func cleanStr(str string) string {
+
 }
